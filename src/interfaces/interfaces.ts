3f736eeef6123a7761c76d6ce4c920f0a30e4e44
Kenny He <kennyjiaxi@hotmail.com>
2024-10-04 14:46:12 -0700

    Create related interfaces

---
+++ src/interfaces/interfaces.ts
@@ -0,0 +1,57 @@
+export interface Party {
+    name:string;
+    address?:string;
+}
+
+export interface Supplier extends Party {
+    tradeName?: string; 
+    taxId?: number; 
+    phoneNumber?: string; 
+    email?: string; 
+    website?: string; 
+    country?: string; 
+    annualBillingUSD?: number; 
+    lastEdited?: Date; 
+  }
+
+
+//OFAC Related Interfaces
+export interface SanctionedPartyOFAC extends Party {
+    type: PartyType;
+    program: string;
+    list: OFACListType;
+    score: number;
+}
+
+export interface PartyType {
+    string: 'Entity' | 'Individual';
+}
+
+export interface OFACListType {
+    string: 'SDN' | 'Non-SDN';
+}
+
+
+//World Bank Listing Related Interfaces
+export interface DebarredPartyWBG extends Party {
+    country: string;
+    fromDate: Date;
+    toDate: Date;
+    grounds: string;
+}
+
+
+//Offshore Leaks Related Interfaces
+export interface OffshoreLeakParty {
+    entity: string;
+    jurisdiction: string;
+    linkedTo: string;
+    dataFrom: Date;
+}    
+
+
+
+
+
+
+
